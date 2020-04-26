0b2518366915432230576ccb829ccdc7143d507a
Arno De Koninck <60197976+ArnoDeKoninck@users.noreply.github.com>
2020-04-26 23:05:40 +0200

    Delete script.js

--- sections/sectie-cursisten/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('cursBtn').onclick = function() {
-    var el = document.getElementById('cursistenContainer');
-    if (el.style.display != 'block') {
-        el.style.display = 'block';
-    } else {
-        el.style.display = 'none';
-    };
-};
